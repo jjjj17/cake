d3ce9fc2c7cda10cbbd7a8d5ba0e58fa5e81bc38
javierjavier <javier.rojas1@walmart.com>
2020-08-05 12:01:07 -0400

    working solution but with several returns

--- g.py
+++ g.py
@@ -3,7 +3,7 @@ import itertools
 
 def word(w):
     print('solution start')
-    newset = set()
+    unique_combo= set()
     l = []
     letters = [letter for letter in w]
     blank = ''
@@ -12,10 +12,20 @@ def word(w):
     for i in range(1,len(w)):
         for x in itertools.combinations_with_replacement(w, i):
             combo_string = blank.join(x)
-            if combo_string in w and len(combo_string)<=(len(w)/2):
-                list_of_combos.append(combo_string)
-    unique_combos = set(list_of_combos)
-    print(unique_combos)
+            if combo_string in w and len(combo_string)<=(len(w)/2): #bugueado porque hay de len mayor
+                if combo_string[0:i] == w[0:i]: #ojo que estoy usando el mismo numero en los dos loops, genera combos de tamaño i y recorre de tamaño i igual
+                    if combo_string[0:i] == w[i:i+len(combo_string[0:i])]:
+                        unique_combo.add(combo_string[0:i])
+    for sol in unique_combo:
+        if len(w)/w.count(sol) == len(sol):
+            print(w.count(sol))
+            print(sol)
+        else:
+            print(1)
+            print('no solutions')
+                        #list_of_combos.append(combo_string)
+    #unique_combos = set(list_of_combos)
+    #print(unique_combos)
 
 """     print('trabajanding')
     print(len(l))
@@ -40,4 +50,4 @@ def word(w):
 
 
 
-word("abcabcabcabcabc")
+word("abababababab")

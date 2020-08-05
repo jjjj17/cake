695a51d3bb5ca8f8f3838869a0caf89f809a3e38
javierjavier <javier.rojas1@walmart.com>
2020-08-05 17:03:22 -0400

    working mvp

--- g.py
+++ g.py
@@ -2,47 +2,34 @@
 import itertools
 
 def word(w):
-    print('solution start')
     unique_combo= set()
-    l = []
-    letters = [letter for letter in w]
     blank = ''
-    list_of_combos = []
-    print('trabajanding')
+    combo_dict = {}
+    max_value_keys = []
     for i in range(1,len(w)):
         for x in itertools.combinations_with_replacement(w, i):
             combo_string = blank.join(x)
-            if combo_string in w and len(combo_string)<=(len(w)/2): #bugueado porque hay de len mayor
-                if combo_string[0:i] == w[0:i]: #ojo que estoy usando el mismo numero en los dos loops, genera combos de tamaño i y recorre de tamaño i igual
+            print(combo_string)
+            if combo_string in w and len(combo_string)<=(len(w)/2):
+                if combo_string[0:i] == w[0:i]:
                     if combo_string[0:i] == w[i:i+len(combo_string[0:i])]:
-                        unique_combo.add(combo_string[0:i])
-    for sol in unique_combo:
-        if len(w)/w.count(sol) == len(sol):
-            print(w.count(sol))
-            print(sol)
-        else:
-            print(1)
-            print('no solutions')
-                        #list_of_combos.append(combo_string)
-    #unique_combos = set(list_of_combos)
-    #print(unique_combos)
+                        for x in range(0,len(w),len(x)):
+                            unique_combo.add(combo_string[0:i])
+                            combo_dict[combo_string[0:i]] = w.count(combo_string[0:i])
 
-"""     print('trabajanding')
-    print(len(l))
-    for combo in l:
-        combo_string = blank.join(combo)
-        if combo_string in w and len(combo_string)<=(len(w)/2):
-            list_of_combos.append(combo_string)
-        newset = set(val for val in list_of_combos)
-    print(newset)
-    for x in newset:
-        for i in range(1,len(w)):
-            if x[0:i] == w[0:i]:
-                if x[0:i] == w[i:i+len(x)]:
-                    print('yes!')
-                    print(w.count(x[0:i]))
-        if len(w)/w.count(x) == len(x):
-            print(w.count(x)) """
+                        max_value_keys = [key for key in combo_dict.keys() if combo_dict[key] == max(combo_dict.values())]
+
+
+    for sol in unique_combo:
+        for z in range(0,len(w), len(sol)):
+            w[0:z]
+        if len(w)/w.count(sol) == len(sol):
+            print('cantidad de veces que se repite en la secuencia: ' + str(w.count(sol)))
+            print('extracto de texto repetido: ' + sol)
+        else:
+            print('cantidad de divisiones de la torta: ' + str(1))
+            print('no solutions')
+    print(max_value_keys)
 
 
 
@@ -50,4 +37,32 @@ def word(w):
 
 
 
-word("abababababab")
+#word("abababcdcdcd")
+#word("abababab")
+def a(word):
+    combination = {}
+    for i in range(1, (len(word)//2)+1):
+        if word[:i] in (word[i:]):
+            if len(word)/word.count(word[:i]) == len(word[:i]):
+                combination[word[:i]] = word.count(word[:i])
+            else:
+                pass
+    max_value_keys = [count for combo, count in combination.items() if combination[combo] == max(combination.values())]
+    print(max_value_keys)
+    print(combination)
+        #for j in range(0, len(word), i):
+            #ext = word[j:j+i]
+            #print(j)
+            #print(ext)
+    """ larg = len(word)
+    for i in range(1,len(word)):
+        myset = set()
+        print('iter 1: '+ str(i))
+        for x in range(0,(len(word)//i)):
+            print('iter 2: ' + str(x))
+            myset.add(word[x:x+i])
+            print('extracto: ' + word[x:x+i])
+        print(myset) """
+
+
+a('abcabcabcabc')
